253356742687f7af63278fb3a5916bbe29b6e163
dylan <nyskyline7@gmail.com>
2023-07-26 17:28:36 +0900

    Add error handling of multiple JSON.parse operations in handlePostRequest

--- index.js
+++ index.js
@@ -35,27 +35,34 @@ function handlePostRequest(request, response) {
     });
 
     request.on('end', async () => {
-        const parsedBody = JSON.parse(body);
-        const responseData = { message: 'Received data successfully! (POST)', data: parsedBody };
-        /*
-        * JS passes objects and arrays by reference. 
-        * To mitigate, create deep copy of parsedBody as below.
-        */
-        const parsedBodyCopy = JSON.parse(JSON.stringify(parsedBody));
-
-        // Add CREATOR_NAME to clubMembers in parsedBodyCopy
-        parsedBodyCopy.clubMembers.push(CREATOR_NAME);
-        const parsedBodyWithCreatorName = JSON.stringify(parsedBodyCopy, null, 2);
-
         try {
-            await fs.writeFile('./club.json', parsedBodyWithCreatorName);
-            response.writeHead(200, { 'Content-Type': 'application/json;  charset=utf-8' });
-            response.end(JSON.stringify(responseData));
+            const parsedBody = JSON.parse(body);
+            /*
+            * JS passes objects and arrays by reference. 
+            * To mitigate, create deep copy of parsedBody as below.
+            */
+            const parsedBodyCopy = JSON.parse(JSON.stringify(parsedBody));
+            const responseData = { message: 'Received data successfully! (POST)', data: parsedBody };
+
+            // Add CREATOR_NAME to clubMembers in parsedBodyCopy
+            parsedBodyCopy.clubMembers.push(CREATOR_NAME);
+            const parsedBodyWithCreatorName = JSON.stringify(parsedBodyCopy, null, 2);
+
+            try {
+                await fs.writeFile('./club.json', parsedBodyWithCreatorName);
+                response.writeHead(200, { 'Content-Type': 'application/json;  charset=utf-8' });
+                response.end(JSON.stringify(responseData));
+            } catch(err) {
+                console.log('Error writing request body to JSON:', err);
+                response.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+                response.end('Internal Server Error');
+            }
         } catch(err) {
-            console.log('Error writing request body to JSON:', err);
+            console.log('error parsing received JSON', err);
             response.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
             response.end('Internal Server Error');
         }
+        
     });
 }
 
